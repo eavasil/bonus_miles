41767f67eadbb67e934538ff81509481c4f717ea
eavasil <dan_kat@mail.ru>
2022-03-26 23:20:35 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 120_000;
+        int bonusMiles = ticketPrice/20;
+        System.out.println("За текущий перелет вам начилено " + bonusMiles + " миль");
+    }
+}
